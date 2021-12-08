d30ca003860fbe30b63b8763fbb57b55ce4e2155
Professional <m.fedarenka@mail.ru>
2021-12-08 21:19:27 +0300

    new commands

--- run.bat
+++ run.bat
@@ -4,3 +4,18 @@ REM This is another comment
 
 :: --- CHANGE DIR ---
 cd /D "D:\java\my-app" & :: /D allows to change drive and dir at the same time
+
+:: --- TIMEOUTS ---
+TIMEOUT /T 600
+
+:: --- OUTPUT ---
+echo. & :: prints an empty line
+echo hello world & :: prints hello world phrase
+
+:: --- MAVEN ---
+cd /D "D:\Java\prj-1"
+call mvn clean install & :: call allows to execute several command in one run
+cd /D "D:\Java\prj-2"
+call mvn clean package
+
+exit & :: close and exit
